7616075c97ba24ddc577048f208a13e63ca58aaa
hz-qq2 <601661706@qq.com>
2019-06-14 08:14:52 +0800

    删除

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-import {store} from './redux/store';
-import 'antd/dist/antd.css';
-import { createApp } from './app';
-
-
-const App = createApp(store)
-
-ReactDOM.render(App, document.getElementById('root'));
--- src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import {createStore,combineReducers} from 'redux';
-import {addRedux} from './reducer/addReducer';
-import {tyyReducer} from './reducer/tyyReducer';
-
-export const store = createStore(combineReducers({addRedux,tyyReducer}));
