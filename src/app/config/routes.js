8fd8f7825796d407e453b2dcf94ecc225de826fa
hz-qq2 <601661706@qq.com>
2019-07-04 01:05:12 +0800

    处理login.js等eslint问题，配置相关eslint规则

--- src/app/config/routes.js
+++ src/app/config/routes.js
@@ -52,10 +52,6 @@ const authorizedRoutes = [{
   unauthorized: Unauthorized,
 }];
 
-//这里需要定义两个路由？，绝对路径时，当登录后，重定向/outlets，
-// 而/outlets是需要鉴权的页面，如果没有登录，会自动重定向到/login
-//所以这里要定义/login，并且把登录页面指向到这个路由；如果不定义/login将指向404页面。
-//所以这里必须要定义绝对路径和/login
 const normalRoutes = [{
   path: '/',
   exact: true,
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,31 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import isEmpty from 'lodash/isEmpty';
 import Cookie from 'js-cookie';
-import { Input, Icon, Button } from 'antd';
+import { Input, Icon, Button, } from 'antd';
 import logo from 'assets/logo.svg';
 import loginAction from './redux/loginAction';
 import './index.scss';
-import {messages,buildConfig, } from '../../app/config/buildConfig';
 import connectWills from '../../utils/connectWills';
 
 const propTypes = {
   prefixCls: PropTypes.string,
-  intl: PropTypes.object,
+  intl: PropTypes.shape({
+    updateLocale: PropTypes.func,
+    formatMessage: PropTypes.func,
+    locale: PropTypes.string,
+  }),
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
   errorMsg: PropTypes.string,
   isLogin: PropTypes.bool,
-  loginUser: PropTypes.func,
-  history: PropTypes.object,
+  app: PropTypes.object,
+  appAction: PropTypes.object,
 };
 
 const defaultProps = {
   prefixCls: 'view-login',
+  intl: {
+    updateLocale: undefined,
+    formatMessage: undefined,
+    locale: undefined
+  },
+  history: {
+    push: undefined
+  },
+  errorMsg: undefined,
+  isLogin: false,
+  app: {},
+  appAction: {},
 };
 
-const componentName = 'app';
-
-@connectWills(componentName, loginAction, {isInjectIntl:true, iswithRouter:true})
+@connectWills('app', loginAction, { isInjectIntl: true, iswithRouter: true })
 export default class Login extends Component {
   state = {
     username: '',
@@ -33,49 +49,46 @@ export default class Login extends Component {
   };
 
   componentDidMount() {
-    const { history } = this.props;
-    const { isLogin } = this.props[componentName];
-    
+    const { history, } = this.props;
+    const { isLogin, } = this.props.app;
     if (isLogin) {
-      //实现页面跳转，参考：https://segmentfault.com/a/1190000011137828
       history.push('/');
     }
   }
 
   componentDidUpdate() {
-    const { history } = this.props;
-    const { isLogin } = this.props[componentName];
+    const { history, } = this.props;
+    const { isLogin, } = this.props.app;
     if (isLogin) {
       history.push('/');
     }
   }
 
   onInputChange = (e, key) => {
-    this.setState({ [key]: e.target.value });
+    this.setState({ [key]: e.target.value, });
   }
 
   handleLogin = () => {
-    const { post } = this.props.appAction;
-    const { username, password } = this.state;
-    post('APP_LOGIN', { username, password }, {
-      successHanlder: res=>{
+    const { post, } = this.props.appAction;
+    const { username, password, } = this.state;
+    post('APP_LOGIN', { username, password, }, {
+      successHanlder: (res) => {
         Cookie.set('user', JSON.stringify(res));
         post('notices');
       },
-      errorHandler: err=>{
-        console.error(err);
-        this.props.appAction.resetLoginErrorMsg()
-      }
-    })
+      errorHandler: (err) => {
+        this.props.appAction.resetLoginErrorMsg(err);
+      },
+    });
   }
 
   updateLocale = (locale) => {
-    const { intl } = this.props;
+    const { intl, } = this.props;
     intl.updateLocale(locale);
   }
 
   renderErrorMsg = () => {
-    const { errorMsg, prefixCls } = this.props;
+    const { errorMsg, prefixCls, } = this.props;
     const show = !isEmpty(errorMsg);
     if (show) {
       return (
@@ -88,35 +101,34 @@ export default class Login extends Component {
   };
 
   renderLoginPanel = () => {
-    const { prefixCls, intl } = this.props;
-    const { username, password } = this.state;
+    const { prefixCls, intl, } = this.props;
+    const { username, password, } = this.state;
     return (
       <div className={`${prefixCls}-loginPanel`}>
         <div className={`${prefixCls}-appInfo`}>
           <img className={`${prefixCls}-appLogo`} src={logo} alt="logo" />
           <span className={`${prefixCls}-appName`}>
-            {intl.formatMessage({ id: 'appName' })}
+            {intl.formatMessage({ id: 'appName', })}
           </span>
         </div>
         <div className={`${prefixCls}-appDesc`}>
-          {intl.formatMessage({ id: 'login_appDesc' })}
+          {intl.formatMessage({ id: 'login_appDesc', })}
         </div>
         <Input
           className={`${prefixCls}-loginInput`}
-          style={{ height: 40, marginBottom: 24 }}
-          placeholder={intl.formatMessage({ id: 'login_usernameInput_placeholder' })}
+          style={{ height: 40, marginBottom: 24, }}
+          placeholder={intl.formatMessage({ id: 'login_usernameInput_placeholder', })}
           type="text"
-          prefix={<Icon type="user" style={{ color: 'rgba(0, 0, 0, .25)' }} />}
+          prefix={<Icon type="user" style={{ color: 'rgba(0, 0, 0, .25)', }} />}
           value={username}
           onChange={e => this.onInputChange(e, 'username')}
-          //onPressEnter 当光标在输入框内时，然后敲回车键就会触发
           onPressEnter={this.handleLogin}
         />
         <Input
           className={`${prefixCls}-loginInput`}
-          placeholder={intl.formatMessage({ id: 'login_passwordInput_placeholder' })}
+          placeholder={intl.formatMessage({ id: 'login_passwordInput_placeholder', })}
           type="password"
-          prefix={<Icon type="lock" style={{ color: 'rgba(0, 0, 0, .25)' }} />}
+          prefix={<Icon type="lock" style={{ color: 'rgba(0, 0, 0, .25)', }} />}
           value={password}
           onChange={e => this.onInputChange(e, 'password')}
           onPressEnter={this.handleLogin}
@@ -126,7 +138,7 @@ export default class Login extends Component {
           type="primary"
           onClick={this.handleLogin}
         >
-          {intl.formatMessage({ id: 'login_login_btn' })}
+          {intl.formatMessage({ id: 'login_login_btn', })}
         </Button>
         <div>
           {this.renderErrorMsg()}
@@ -136,9 +148,7 @@ export default class Login extends Component {
   }
 
   renderIntlSwitch = () => {
-    const { prefixCls, intl ,history} = this.props;
-    const { locale } = buildConfig;
-
+    const { prefixCls, intl, } = this.props;
     return (
       <div className={`${prefixCls}-intlSwitch`}>
         <span
@@ -164,22 +174,12 @@ export default class Login extends Component {
         >
           中文
         </span>
-        <div onClick={()=>{
-          // console.log(JSON.stringify(messages));
-          // console.log(JSON.stringify(locale));
-          console.log(this.props.loginUser);
-          // history.push('/kkkkk')
-        }}>oooooooooo</div>
-        <Acv></Acv>
       </div>
-
     );
   }
 
   render() {
-    const { prefixCls } = this.props;
-    console.log(this.props)
-    console.log(this.props.appAction)
+    const { prefixCls, } = this.props;
     return (
       <div className={prefixCls}>
         {this.renderLoginPanel()}
@@ -188,12 +188,6 @@ export default class Login extends Component {
     );
   }
 }
-
-
-const Acv = ({aaa})=>{
-  return (<div>{aaa}</div>)
-}
-Acv.defaultProps={aaa:999889}
 
 Login.propTypes = propTypes;
 Login.defaultProps = defaultProps;
